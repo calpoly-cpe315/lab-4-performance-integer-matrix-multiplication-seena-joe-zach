15746cd10f89097a941a88a1ba3d37984ecbf107
jogara2001 <71284355+jogara2001@users.noreply.github.com>
2022-02-28 21:17:26 -0800

    Add files via upload

---
+++ intsub.s
@@ -0,0 +1,25 @@
+    // intsub function in this file
+
+    .arch armv8-a
+    .global intsub
+
+intsub:
+    stp x29,x30,[sp,-32]!
+    stp x19,x20,[sp,16]
+    mvn x19,x0
+    sxtw x0,w0
+    sxtw x1,w1
+if:
+    cmp x1,0
+    bne else
+    b endif
+else:
+    EOR x0,x0,x1
+    AND x1,x19,x1
+    LSL x1,x1,1
+    bl intsub
+endif:
+    ldp x19,x20,[sp,16]
+    ldp x29,x30,[sp],32
+    ret
+
