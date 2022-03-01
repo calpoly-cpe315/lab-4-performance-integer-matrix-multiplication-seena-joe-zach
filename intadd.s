8ebd8b38e1436cbfac3b039293b7447f902e9bc0
ZRiley36 <zachriley36@gmail.com>
2022-02-28 19:06:20 -0800

    copied our existing intadd.s and matul.s functions from lab-3

--- intadd.s
+++ intadd.s
@@ -4,4 +4,21 @@
     .global intadd
 
 intadd:
-
+    stp x29,x30,[sp,-32]!
+    stp x19,x20,[sp,16]
+    sxtw x0,w0
+    sxtw x1,w1
+if:
+    cmp x1,0
+    bne else
+    b endif
+else:
+    mov x19,x0
+    EOR x0,x0,x1
+    AND x1,x19,x1
+    LSL x1,x1,1
+    bl intadd
+endif:
+    ldp x19,x20,[sp,16]
+    ldp x29,x30,[sp],32
+    ret
--- intmul.s
+++ intmul.s
@@ -4,3 +4,49 @@
 
 intmul:
 
+    stp    x29, x30, [sp,-48]!
+    stp    x19, x20, [sp, 16]
+    stp    x21, x22, [sp, 32]
+    
+
+    mov x19, x0  //a
+    mov x20, x1  //b
+
+    cmp x19, #0
+    beq zero
+
+    cmp x20, #0
+    beq zero
+    
+    mov x22, 0 //total
+loop:
+    // add a to total
+    mov x0, x22
+    mov x1, x19
+    bl intadd
+    mov x22, x0
+    
+    //subtract 1 from b
+    mov x0, x20
+    mov x1, 1
+    bl intsub
+    mov x20, x0
+
+    cmp x20, 0
+    b.eq finish
+    b loop
+    
+
+zero:
+    mov x0, #0
+    b finish
+
+
+finish:
+    mov x0, x22
+    ldp    x19, x20, [sp, 16]
+    ldp    x21, x22, [sp, 32]
+    ldp    x29, x30, [sp], 48    
+    ret
+
+
