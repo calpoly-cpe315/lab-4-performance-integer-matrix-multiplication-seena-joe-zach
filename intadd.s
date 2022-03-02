5726fe37792ddaf2996f375c59b0be1516572790
Zach Riley <zriley@arm.csc.calpoly.edu>
2022-03-01 21:34:26 -0500

    updated intadd.s

--- intadd.s
+++ intadd.s
@@ -1,24 +1,22 @@
-//   Replace this file with your own implementation
-
-    .arch armv8-a
-    .global intadd
-
+.arch armv8-a
+.global intadd
 intadd:
-    stp x29,x30,[sp,-32]!
-    stp x19,x20,[sp,16]
-    sxtw x0,w0
-    sxtw x1,w1
-if:
-    cmp x1,0
-    bne else
-    b endif
-else:
-    mov x19,x0
-    EOR x0,x0,x1
-    AND x1,x19,x1
-    LSL x1,x1,1
-    bl intadd
-endif:
-    ldp x19,x20,[sp,16]
-    ldp x29,x30,[sp],32
-    ret
+/* Push to stack */
+   stp x29, x30, [sp, -32]!
+   add x29, sp, 0
+   stp x19, x20, [sp, 16]
+loop:
+/* Removed s from eor, and, lsl. Aarch64 compiler didn't
+* like it */
+   eor x19, x0, x1 /* Exclusive or */
+   and x20, x0, x1 /* Get the carry */
+   lsl x20, x20, 1 /* Shift carry to left by one                              */
+   mov x0, x19 /* Put XOR or result in x0 */
+   mov x1, x20 /* Put carry in x1 */
+   cmp x1, 0 /* If carry is 0, addition is complete */
+   bne loop
+            /* Pop off stack */
+   ldp x19, x20, [sp, 16]
+   ldp x29, x30, [sp], 32
+   /* Return */
+   ret
