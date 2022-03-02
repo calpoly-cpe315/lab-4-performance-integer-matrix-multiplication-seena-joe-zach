c7790173cd7b4eb4650bd0295ae8b20659214965
ZRiley36 <zachriley36@gmail.com>
2022-03-01 21:10:08 -0800

    changed intmul to take care of negatives

--- intmul.s
+++ intmul.s
@@ -1,52 +1,91 @@
 // Replace this file with your own implementation
-	.arch armv8-a
-	.global intmul
-
+    .arch armv8-a
+    .global intmul
+ 
 intmul:
-
+ 
     stp    x29, x30, [sp,-48]!
     stp    x19, x20, [sp, 16]
     stp    x21, x22, [sp, 32]
-    
-
+   
+ 
     mov x19, x0  //a
     mov x20, x1  //b
-
+ 
+    mov x22, 0 //total
+   
     cmp x19, #0
     beq zero
-
+ 
     cmp x20, #0
     beq zero
-    
-    mov x22, 0 //total
+    bge loop
+ 
+    cmp x19, #0
+    bge loop2
+ 
+    b loop3
+   
+ 
 loop:
     // add a to total
     mov x0, x22
     mov x1, x19
     bl intadd
     mov x22, x0
-    
+   
     //subtract 1 from b
     mov x0, x20
     mov x1, 1
     bl intsub
     mov x20, x0
-
-    cmp xzr, x20
-    bge finish
+ 
+    cmp x20, 0
+    b.eq finish
     b loop
-    
-
+   
+loop2:
+    // add a to total
+    mov x0, x22
+    mov x1, x19
+    bl intadd
+    mov x22, x0
+   
+    //subtract 1 from b
+    mov x0, x20
+    mov x1, 1
+    bl intadd
+    mov x20, x0
+ 
+    cmp x20, 0
+    b.eq finish
+    b loop2
+ 
+loop3:
+    // add a to total
+    mov x0, x22
+    mov x1, x19
+    bl intsub
+    mov x22, x0
+   
+    //subtract 1 from b
+    mov x0, x20
+    mov x1, 1
+    bl intadd
+    mov x20, x0
+ 
+    cmp x20, 0
+    b.eq finish
+    b loop3
+ 
 zero:
     mov x22, #0
     b finish
-
-
+ 
+ 
 finish:
     mov x0, x22
     ldp    x19, x20, [sp, 16]
     ldp    x21, x22, [sp, 32]
     ldp    x29, x30, [sp], 48    
     ret
-
-
